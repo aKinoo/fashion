5cba09d0948fc60b51f0f3426fa8d07513166eaa
Kinoo <Kinoo>
2016-07-05 17:34:16 +0900

    Initial commit

---
+++ app/src/main/java/com/example/admin/fashion/MainActivity.java
@@ -0,0 +1,18 @@
+package com.example.admin.fashion;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.ImageView;
+
+public class MainActivity extends AppCompatActivity {
+
+    ImageView imageView1;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        imageView1 = (ImageView)findViewById(R.id.imageView);
+        imageView1.setImageResource(R.drawable.Tanktop);
+
+    }
+}
