74c03a5d022fdbeb36dae44998e8c7c039741993
HitoIo <hr_pira09pine56@rb4.so-net.ne.jp>
2016-08-03 12:47:30 +0900

    蓮池さんプログラム合流

--- app/src/main/java/com/example/admin/fashion/Tree.java
+++ app/src/main/java/com/example/admin/fashion/Tree.java
@@ -11,5 +11,17 @@ public class Tree {
     private void setTrainData(){
         //0天気（0晴れ,1くもり,2雨）,1温度(0:15以下,1:15~25,2:25以上),2スケジュール(0何もない,1デート,2友達,3バイト),
         // 3結果(0harfpants,1longpants,2longskirt,3shortskirt,4one-piece)
+        int[][] tData = {{0,2,0,0},{1,2,0,3},{2,2,0,0},     //学校
+                {0,1,1,4},                                  //デート
+                {1,0,3,1},                                  //バイト
+                {0,0,2,2},{1,1,2,2},{2,2,2,3}};
+        int len = tData.length;
+        for(int i = 0; i<len;i++){
+            trainData.add(tData[i]);
+        }
+    }
+
+    private float entropy(){
+        return 0;
     }
 }
