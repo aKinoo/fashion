165965423e2cf595bf7cdb5ff06288a7fed68e2f
aKinoo <Kinoo0616@gmail.com>
2016-08-03 19:29:32 +0900

    images

--- app/src/main/java/com/example/admin/fashion/ShowFashion.java
+++ app/src/main/java/com/example/admin/fashion/ShowFashion.java
@@ -14,6 +14,7 @@ import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
+import android.widget.ImageButton;
 import android.widget.ImageView;
 
 
@@ -35,30 +36,125 @@ public class ShowFashion extends AppCompatActivity {
         setView();
     }
 
-    public void setView() {
-        ImageView image_top = (ImageView) findViewById(R.id.image_top);
+    public void setView(){
+        final ImageView image_top = (ImageView)findViewById(R.id.image_top);
         image_top.setImageResource(tops());
-        ImageView image_bottom = (ImageView) findViewById(R.id.image_bottom);
+        final ImageView image_bottom = (ImageView)findViewById(R.id.image_bottom);
         image_bottom.setImageResource(bottoms());
-
-        final CanvasView canvas_top = (CanvasView) this.findViewById(R.id.canvas_top);
-        final CanvasView canvas_bottom = (CanvasView) this.findViewById(R.id.canvas_bottom);
-
+        //柄表示
+        final ImageView design_top = (ImageView)findViewById(R.id.design_top);
+        final ImageView design_bottom = (ImageView)findViewById(R.id.design_bottom);
+        ImageButton top_design0 = (ImageButton)findViewById(R.id.top_design0);
+        top_design0.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                design_top.setVisibility(View.INVISIBLE);
+            }
+        });
+        ImageButton top_design1 = (ImageButton)findViewById(R.id.top_design1);
+        top_design1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                design_top.setVisibility(View.VISIBLE);
+                design_top.setImageResource(R.drawable.lblack);
+            }
+        });
+        ImageButton top_design2 = (ImageButton)findViewById(R.id.top_design2);
+        top_design2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                design_top.setVisibility(View.VISIBLE);
+                design_top.setImageResource(R.drawable.pinckflower);
+            }
+        });
+        ImageButton top_design3 = (ImageButton)findViewById(R.id.top_design3);
+        top_design3.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                design_top.setVisibility(View.VISIBLE);
+                design_top.setImageResource(R.drawable.sblack);
+            }
+        });
+        ImageButton top_design4 = (ImageButton)findViewById(R.id.top_design4);
+        top_design4.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                design_top.setVisibility(View.VISIBLE);
+                design_top.setImageResource(R.drawable.swhite);
+            }
+        });
+        //ボトムス柄ボタン
+        ImageButton bottom_design0 = (ImageButton)findViewById(R.id.bottom_design0);
+        bottom_design0.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                design_bottom.setVisibility(View.INVISIBLE);
+            }
+        });
+        ImageButton bottom_design1 = (ImageButton)findViewById(R.id.bottom_design1);
+        bottom_design1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                design_bottom.setVisibility(View.VISIBLE);
+                design_bottom.setImageResource(R.drawable.lblack);
+            }
+        });
+        ImageButton bottom_design2 = (ImageButton)findViewById(R.id.bottom_design2);
+        bottom_design2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                design_bottom.setVisibility(View.VISIBLE);
+                design_bottom.setImageResource(R.drawable.pinckflower);
+            }
+        });
+        ImageButton bottom_design3 = (ImageButton)findViewById(R.id.bottom_design3);
+        bottom_design3.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                design_bottom.setVisibility(View.VISIBLE);
+                design_bottom.setImageResource(R.drawable.sblack);
+            }
+        });
+        ImageButton bottom_design4 = (ImageButton)findViewById(R.id.bottom_design4);
+        bottom_design4.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                design_bottom.setVisibility(View.VISIBLE);
+                design_bottom.setImageResource(R.drawable.swhite);
+            }
+        });
+        ImageButton bottom_design5 = (ImageButton)findViewById(R.id.bottom_design5);
+        bottom_design5.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                design_bottom.setVisibility(View.VISIBLE);
+                design_bottom.setImageResource(R.drawable.brown);
+            }
+        });
+        final CanvasView canvas_top = (CanvasView)this.findViewById(R.id.canvas_top);
+        final CanvasView canvas_bottom = (CanvasView)this.findViewById(R.id.canvas_bottom);
         SharedPreferences pref = PreferenceManager.getDefaultSharedPreferences(ShowFashion.this);
 
         int[][] color = canvas_top.getColor();
         final int[] top_color = new int[4];
-        for (int i = 0; i < 4; i++) {
+        for(int i = 0; i < 4;i++){
 //            Log.d("main",SettingPrefActivity.PREF_COLOR_SETTING[i]);
 //            Log.d("main",pref.getString(SettingPrefActivity.PREF_COLOR_SETTING[i],"0"));
             String num = i + "";
-            String c = pref.getString(SettingPrefActivity.PREF_COLOR_SETTING[i], num);
+            String c = pref.getString(SettingPrefActivity.PREF_COLOR_SETTING[i],num);
 //            Log.d("main",c);
             top_color[i] = Integer.parseInt(c);
         }
+        final int[] bottom_color = new int[4];
+        for(int i = 4;i<8;i++){
+            String num = i + "";
+            String c = pref.getString(SettingPrefActivity.PREF_COLOR_SETTING[i],num);
+//            Log.d("main",c);
+            bottom_color[i-4] = Integer.parseInt(c);
+        }
         //トップスカラーボタン
         Button top_color1 = (Button) findViewById(R.id.top_color1);
-        top_color1.setBackgroundColor(Color.argb(125, color[top_color[0]][0], color[top_color[0]][1], color[top_color[0]][2]));
+        top_color1.setBackgroundColor(Color.argb(125,color[top_color[0]][0],color[top_color[0]][1],color[top_color[0]][2]));
         top_color1.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
@@ -66,7 +162,7 @@ public class ShowFashion extends AppCompatActivity {
             }
         });
         Button top_color2 = (Button) findViewById(R.id.top_color2);
-        top_color2.setBackgroundColor(Color.argb(125, color[top_color[1]][0], color[top_color[1]][1], color[top_color[1]][2]));
+        top_color2.setBackgroundColor(Color.argb(125,color[top_color[1]][0],color[top_color[1]][1],color[top_color[1]][2]));
         top_color2.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
@@ -74,7 +170,7 @@ public class ShowFashion extends AppCompatActivity {
             }
         });
         Button top_color3 = (Button) findViewById(R.id.top_color3);
-        top_color3.setBackgroundColor(Color.argb(125, color[top_color[2]][0], color[top_color[2]][1], color[top_color[2]][2]));
+        top_color3.setBackgroundColor(Color.argb(125,color[top_color[2]][0],color[top_color[2]][1],color[top_color[2]][2]));
         top_color3.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
@@ -82,69 +178,68 @@ public class ShowFashion extends AppCompatActivity {
             }
         });
         Button top_color4 = (Button) findViewById(R.id.top_color4);
-        top_color4.setBackgroundColor(Color.argb(125, color[top_color[3]][0], color[top_color[3]][1], color[top_color[3]][2]));
+        top_color4.setBackgroundColor(Color.argb(125,color[top_color[3]][0],color[top_color[3]][1],color[top_color[3]][2]));
         top_color4.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 canvas_top.showCanvas(top_color[3]);
             }
         });
-
         //ボトムスカラーボタン
-        Button bottom_color1 = (Button) findViewById(R.id.bottom_color1);
-        bottom_color1.setBackgroundColor(Color.argb(125, color[1][0], color[1][1], color[1][2]));
-        bottom_color1.setOnClickListener(new View.OnClickListener() {
+        Button bottom_color1 = (Button)findViewById(R.id.bottom_color1);
+        bottom_color1.setBackgroundColor(Color.argb(125,color[bottom_color[0]][0],color[bottom_color[0]][1],color[bottom_color[0]][2]));
+        bottom_color1.setOnClickListener(new View.OnClickListener(){
             @Override
-            public void onClick(View v) {
-                canvas_bottom.showCanvas(1);
+            public void onClick(View v){
+                canvas_bottom.showCanvas(bottom_color[0]);
             }
         });
-        Button bottom_color2 = (Button) findViewById(R.id.bottom_color2);
-        bottom_color2.setBackgroundColor(Color.argb(125, color[2][0], color[2][1], color[2][2]));
-        bottom_color2.setOnClickListener(new View.OnClickListener() {
+        Button bottom_color2 = (Button)findViewById(R.id.bottom_color2);
+        bottom_color2.setBackgroundColor(Color.argb(125,color[bottom_color[1]][0],color[bottom_color[1]][1],color[bottom_color[1]][2]));
+        bottom_color2.setOnClickListener(new View.OnClickListener(){
             @Override
-            public void onClick(View v) {
-                canvas_bottom.showCanvas(2);
+            public void onClick(View v){
+                canvas_bottom.showCanvas(bottom_color[1]);
             }
         });
-        Button bottom_color3 = (Button) findViewById(R.id.bottom_color3);
-        bottom_color3.setBackgroundColor(Color.argb(125, color[3][0], color[3][1], color[3][2]));
-        bottom_color3.setOnClickListener(new View.OnClickListener() {
+        Button bottom_color3 = (Button)findViewById(R.id.bottom_color3);
+        bottom_color3.setBackgroundColor(Color.argb(125,color[bottom_color[2]][0],color[bottom_color[2]][1],color[bottom_color[2]][2]));
+        bottom_color3.setOnClickListener(new View.OnClickListener(){
             @Override
-            public void onClick(View v) {
-                canvas_bottom.showCanvas(3);
+            public void onClick(View v){
+                canvas_bottom.showCanvas(bottom_color[2]);
             }
         });
-        Button bottom_color4 = (Button) findViewById(R.id.bottom_color4);
-        bottom_color4.setBackgroundColor(Color.argb(125, color[4][0], color[4][1], color[4][2]));
-        bottom_color4.setOnClickListener(new View.OnClickListener() {
+        Button bottom_color4 = (Button)findViewById(R.id.bottom_color4);
+        bottom_color4.setBackgroundColor(Color.argb(125,color[bottom_color[3]][0],color[bottom_color[3]][1],color[bottom_color[3]][2]));
+        bottom_color4.setOnClickListener(new View.OnClickListener(){
             @Override
-            public void onClick(View v) {
-                canvas_bottom.showCanvas(4);
+            public void onClick(View v){
+                canvas_bottom.showCanvas(bottom_color[3]);
             }
         });
-
         //スワイプメニューからトップ画面へ遷移
-        Button drawer_button_top = (Button) findViewById(R.id.drawer_button_top);
-        drawer_button_top.setOnClickListener(new View.OnClickListener() {
+        Button drawer_button_top = (Button)findViewById(R.id.drawer_button_top);
+        drawer_button_top.setOnClickListener(new View.OnClickListener(){
             @Override
-            public void onClick(View v) {
+            public void onClick(View v){
                 finish();
-                Intent intent = new Intent(getApplication(), MainActivity.class);
+                Intent intent = new Intent(getApplication(),MainActivity.class);
                 startActivity(intent);
             }
         });
         //スワイプメニューから過去のファッション画面へ遷移
-        Button drawer_past_fashion = (Button) findViewById(R.id.drawer_past_fashion);
-        drawer_past_fashion.setOnClickListener(new View.OnClickListener() {
+        Button drawer_past_fashion = (Button)findViewById(R.id.drawer_past_fashion);
+        drawer_past_fashion.setOnClickListener(new View.OnClickListener(){
             @Override
-            public void onClick(View v) {
+            public void onClick(View v){
                 finish();
-                Intent intent = new Intent(getApplication(), PastFashion.class);
+                Intent intent = new Intent(getApplication(),PastFashion.class);
                 startActivity(intent);
             }
         });
     }
+
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
