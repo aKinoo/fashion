822bd0befb50c1d7da74f79c8e2dbad0a1443ead
HitoIo <hr_pira09pine56@rb4.so-net.ne.jp>
2016-08-05 00:00:19 +0900

    test

--- app/src/main/java/com/example/admin/fashion/FashionCalender.java
+++ app/src/main/java/com/example/admin/fashion/FashionCalender.java
@@ -46,6 +46,7 @@ public class FashionCalender extends AppCompatActivity {
 
 
 
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
